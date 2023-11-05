9067f368667098628526ff68fae5cab0f2adde71
Azeem Ali <AzeemAli14@github.com>
2023-11-06 01:37:37 +0500

    Boilder Plate code removed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <button class="btn">-</button>
+    <span class="counter">0</span>
+    <button class="btn">+</button>
+  </div>
 </template>
+
+<style scoped>
+.home {
+  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
