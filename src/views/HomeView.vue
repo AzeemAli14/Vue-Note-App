a9a63489cee28a5f69b80b81a8f4f2919ffc4a8d
Azeem Ali <AzeemAli14@github.com>
2023-11-08 03:25:17 +0500

    vue lifecycle hooks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,7 +22,7 @@
 
 <!-- Compsition API -->
 <script setup>
-import { ref, reactive, computed, watch } from 'vue';
+import { ref, reactive, computed, watch, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue';
 
 // Non-Reactive Value
 const appTitle = 'Vue Counter App'
@@ -60,6 +60,33 @@ watch(() => counterData.count, (newVal, oldVal) => {
     alert('10 reached ... Hoorah!')
   }
 })
+
+// Lifecycle Hooks
+onBeforeMount(() => {
+  console.log('onBeforeMount')
+}),
+
+onMounted(() => {
+  console.log('onMounted')
+})
+
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate')
+})
+
+onUpdated(() => {
+  console.log('onUpdated')
+})
+
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount')
+})
+
+onUnmounted(() => {
+  console.log('onUnmounted')
+})
+
+
 </script>
 
 // export default {
