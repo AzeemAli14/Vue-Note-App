1ba726450d3842e77dc8301710c9052e2f028e94
Azeem Ali <AzeemAli14@github.com>
2023-11-12 23:08:22 +0500

    --about-us-page-added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,117 @@
 <template>
-  <div class="about">
-    <h1>About, Vue!</h1>
-  </div>
+  <section class="relative pt-16 bg-blueGray-50">
+    <div class="container mx-auto">
+      <div class="flex flex-wrap items-center">
+        <div class="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
+          <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-500">
+            <img alt="..."
+              src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80"
+              class="w-full align-middle rounded-t-lg" />
+            <blockquote class="relative p-8 mb-4">
+              <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95"
+                class="absolute left-0 w-full block h-95-px -top-94-px"></svg>
+              <h4 class="text-xl font-bold text-black">
+                Great for your awesome project
+              </h4>
+              <p class="text-md font-light mt-2 text-black">
+                Putting together a page has never been easier than matching
+                together pre-made components. From landing pages presentation to
+                login areas, you can easily customise and built your pages.
+              </p>
+            </blockquote>
+          </div>
+        </div>
+
+        <div class="w-full md:w-6/12 px-4">
+          <div class="flex flex-wrap">
+            <div class="w-full md:w-6/12 px-4">
+              <div class="relative flex flex-col mt-4">
+                <div class="px-4 py-5 flex-auto">
+                  <div
+                    class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="text-green-500">
+                      <path
+                        d="M0 32l34.9 395.8L192 480l157.1-52.2L384 32H0zm313.1 80l-4.8 47.3L193 208.6l-.3.1h111.5l-12.8 146.6-98.2 28.7-98.8-29.2-6.4-73.9h48.9l3.2 38.3 52.6 13.3 54.7-15.4 3.7-61.6-166.3-.5v-.1l-.2.1-3.6-46.3L193.1 162l6.5-2.7H76.7L70.9 112h242.2z" />
+                    </svg>
+                  </div>
+                  <h6 class="text-xl mb-1 font-semibold">CSS Components</h6>
+                  <p class="mb-4 text-blueGray-500">
+                    Notus JS comes with a huge number of Fully Coded CSS
+                    components.
+                  </p>
+                </div>
+              </div>
+              <div class="relative flex flex-col min-w-0">
+                <div class="px-4 py-5 flex-auto">
+                  <div
+                    class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                    <svg class="h-8" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                      <path
+                        d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM243.8 381.4c0 43.6-25.6 63.5-62.9 63.5-33.7 0-53.2-17.4-63.2-38.5l34.3-20.7c6.6 11.7 12.6 21.6 27.1 21.6 13.8 0 22.6-5.4 22.6-26.5V237.7h42.1v143.7zm99.6 63.5c-39.1 0-64.4-18.6-76.7-43l34.3-19.8c9 14.7 20.8 25.6 41.5 25.6 17.4 0 28.6-8.7 28.6-20.8 0-14.4-11.4-19.5-30.7-28l-10.5-4.5c-30.4-12.9-50.5-29.2-50.5-63.5 0-31.6 24.1-55.6 61.6-55.6 26.8 0 46 9.3 59.8 33.7L368 290c-7.2-12.9-15-18-27.1-18-12.3 0-20.1 7.8-20.1 18 0 12.6 7.8 17.7 25.9 25.6l10.5 4.5c35.8 15.3 55.9 31 55.9 66.2 0 37.8-29.8 58.6-69.7 58.6z" />
+                    </svg>
+                  </div>
+                  <h6 class="text-xl mb-1 font-semibold">
+                    JavaScript Components
+                  </h6>
+                  <p class="mb-4 text-blueGray-500">
+                    We also feature many dynamic components for React, NextJS,
+                    Vue and Angular.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-6/12 px-4">
+              <div class="relative flex flex-col min-w-0 mt-4">
+                <div class="px-4 py-5 flex-auto">
+                  <div
+                    class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" class="h-8">
+                      <path
+                        d="M0 80v48c0 17.7 14.3 32 32 32H48 96V80c0-26.5-21.5-48-48-48S0 53.5 0 80zM112 32c10 13.4 16 30 16 48V384c0 35.3 28.7 64 64 64s64-28.7 64-64v-5.3c0-32.4 26.3-58.7 58.7-58.7H480V128c0-53-43-96-96-96H112zM464 480c61.9 0 112-50.1 112-112c0-8.8-7.2-16-16-16H314.7c-14.7 0-26.7 11.9-26.7 26.7V384c0 53-43 96-96 96H368h96z" />
+                    </svg>
+                  </div>
+                  <h6 class="text-xl mb-1 font-semibold">Pages</h6>
+                  <p class="mb-4 text-blueGray-500">
+                    This extension also comes with 3 sample pages. They are
+                    fully coded so you can start working instantly.
+                  </p>
+                </div>
+              </div>
+              <div class="relative flex flex-col min-w-0">
+                <div class="px-4 py-5 flex-auto">
+                  <div
+                    class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                    <svg class="h-8" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+                      <path
+                        d="M320 464c8.8 0 16-7.2 16-16V160H256c-17.7 0-32-14.3-32-32V48H64c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320zM0 64C0 28.7 28.7 0 64 0H229.5c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64z" />
+                    </svg>
+                  </div>
+                  <h6 class="text-xl mb-1 font-semibold">Documentation</h6>
+                  <p class="mb-4 text-blueGray-500">
+                    Built by developers for developers. You will love how easy
+                    is to to work with Vue JS.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="relative bg-blueGray-50 pt-8 pb-6 mt-2">
+      <div class="container mx-auto px-4">
+        <div class="flex flex-wrap items-center md:justify-between justify-center">
+          <div class="w-full md:w-6/12 px-4 mx-auto text-center">
+            <div class="text-sm text-blueGray-500 font-semibold py-1">
+              Made with
+              <p class="text-white hover:text-[#41b883]">Vue JS</p>
+              by
+              <a class="text-white hover:text-[#41b883]">
+                Creative Me</a>.
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </section>
 </template>
